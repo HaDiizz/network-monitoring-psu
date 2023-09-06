84bcc9bf40804b2bbc020d6215e7dab77574e4f7
Tanakorn Pongtongmuang <96484902+Tanakorn13@users.noreply.github.com>
2023-09-06 13:20:12 +0700

    use httpx in host_list()

--- app/utils.py
+++ app/utils.py
@@ -4,11 +4,15 @@ from flask_login import current_user
 from . import caches
 import os
 from dotenv import load_dotenv
+import httpx
 
 load_dotenv()
 
 API_URL = f"https://{os.environ['HOST_NAME']}/{os.environ['SITE_NAME']}/check_mk/api/1.0"
-
+HEADERS = {
+    'Authorization': f"Bearer {os.environ['CHECKMK_USERNAME']} {os.environ['CHECKMK_PASSWORD']}",
+    'Accept': 'application/json'
+}
 
 def status_list():
     return [
@@ -26,21 +30,22 @@ def location_list():
 
 def host_list():
     try:
-        session = requests.session()
-        session.headers['Authorization'] = f"Bearer {os.environ['CHECKMK_USERNAME']} {os.environ['CHECKMK_PASSWORD']}"
-        session.headers['Accept'] = 'application/json'
-        response = session.get(
-            f"{API_URL}/domain-types/host/collections/all",
-            params={
-                "columns": ['name', 'state', 'last_state', 'last_time_up', 'last_time_down', 'last_time_unreachable', 'last_state_change'],
-            },
-        )
-        if response.status_code == 200:
-            response = response.json()
-            if response:
-                return response['value']
-        else:
-            return []
+        with httpx.Client() as client:
+            params = {
+                "columns": ['name', 'state', 'last_state', 'last_time_up', 'last_time_down', 'last_time_unreachable', 'last_state_change', 'labels', "groups"],
+            }
+
+            resp = client.get(
+                f"{API_URL}/domain-types/host/collections/all",
+                headers=HEADERS,
+                params=params
+            )
+            if resp.status_code == 200:
+                resp = resp.json()
+                if resp:
+                    return resp['value']
+            else:
+                return []
     except Exception as ex:
         return None
 
