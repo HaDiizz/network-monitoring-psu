2bfe46b0226e3641c375a48d758d0e3ef78882ee
HaDiizz <nattapol.sh@hotmail.com>
2023-09-28 02:30:29 +0700

    [Updated] month link scroll and avatar

--- app/static/js/quarterlyTable.js
+++ app/static/js/quarterlyTable.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $("#quarterly-table").DataTable({
-      "order": [[0, 'desc']],
+      "order": [[2, 'desc']],
       dom: "Bfrtip",
       buttons: ["copy", "csv", "excel", "print", "colvis"],
     });
