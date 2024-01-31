f10728523982cac25f670035c6c31c69f48952dc
HaDiizz <nattapol.sh@hotmail.com>
2024-01-31 15:56:18 +0700

    Removed handler functions

--- app.py
+++ app.py
@@ -1,13 +1,5 @@
 from app import server
-from app.helpers.api import host_down_handler, service_down_handler, accessPoint_down_handler
-from apscheduler.schedulers.background import BackgroundScheduler
-
-scheduler = BackgroundScheduler()
 
 if __name__ == '__main__':
     with server.app_context():
-        scheduler.add_job(host_down_handler, 'interval', seconds=60)
-        scheduler.add_job(service_down_handler, 'interval', seconds=60)
-        scheduler.add_job(accessPoint_down_handler, 'interval', seconds=60)
-        scheduler.start()
         server.run(debug=True)
